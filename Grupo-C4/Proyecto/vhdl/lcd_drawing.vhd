c2a936ffcf54ab018c5ddfacdeaad231822e62b5
Amanda Sin <81495532+Andyuks@users.noreply.github.com>
2022-11-05 14:32:39 +0100

    fix some issues

--- Grupo-C4/Proyecto/vhdl/lcd_drawing.vhd
+++ Grupo-C4/Proyecto/vhdl/lcd_drawing.vhd
@@ -20,17 +20,17 @@ end lcd_drawing;
 
 architecture arq_lcd_drawing of lcd_drawing is
 
-	-- Declaración de estados
-	type estados is (E0, E1, E2, E3, E4, E5, E6, E7, E8, E9);
+	-- DeclaraciÃ³n de estados
+	type estados is (E0, E1, E2, E3, E4, E5, E6, E7, E8, E9, E10, E11);
 	signal EP, ES : estados;
 
-	-- Declaración de señales de control
+	-- DeclaraciÃ³n de seÃ±ales de control
 	signal SEL_DATA, LD_XY, LD_RGB, CL_XY, INC_Y, LD_NPIX, LD_CNPIX, DEC_CNPIX, ALL_PIX: std_logic :='0';
 	signal DRGB: std_logic_vector(15 downto 0);
-
-	-- Declaración de enteros sin signo para contadores
+	signal MUX_PIX: unsigned(16 downto 0);
+	-- DeclaraciÃ³n de enteros sin signo para contadores
 	signal cnt_YROW: unsigned(8 downto 0);
-	signal u_QPIX: unsigned(7 downto 0);
+	signal u_QPIX: unsigned(16 downto 0);
 
 
 	-- Valores de entrada constantes
@@ -51,7 +51,7 @@ architecture arq_lcd_drawing of lcd_drawing is
 	-- ## UNIDAD DE CONTROL ## 
 	-- #######################
 
-	-- Transición de estados (cálculo de estado siguiente)
+	-- TransiciÃ³n de estados (cÃ¡lculo de estado siguiente)
 	SWSTATE: process (EP, DEL_SCREEN, DRAW_FIG, DONE_CURSOR, DONE_COLOUR) begin
 		case EP is
 			when E0 => 	if DEL_SCREEN = '1' then ES <= E1;
@@ -101,22 +101,22 @@ architecture arq_lcd_drawing of lcd_drawing is
 
 
 
-	-- Actualización de EP en cada flanco de reloj (sequential)
+	-- ActualizaciÃ³n de EP en cada flanco de reloj (sequential)
 	SEQ: process (CLK, RESET_L) begin
-		if RESET_L = '0' then EP <= E0; -- reset asíncrono
+		if RESET_L = '0' then EP <= E0; -- reset asÃ­ncrono
 		elsif CLK'event and CLK = '1'  -- flanco de reloj
 			then EP <= ES;             -- Estado Presente = Estado Siguiente
 		end if;
 	end process SEQ;
 
 
-	-- Activación de señales de control: asignaciones combinacionales - valor a se�al
+	-- ActivaciÃ³n de seÃ±ales de control: asignaciones combinacionales - valor a seï¿½al
 	SEL_DATA <= '1' when EP = E0 and DEL_SCREEN = '0' and DRAW_FIG = '1' else '0';
 	LD_XY <= '1' when SEL_DATA = '1' else '0';
 	LD_CNPIX <= '1' when SEL_DATA = '1' else '0';
 	CL_XY <= '1' when EP = E0 and DEL_SCREEN = '1' else '0';
-	LD_NPIX <= '1' when SEL_DATA = '1' or CL_XY = '1' and else '0';
-	LD_RGB <= '1' when LD_NPIX = '1' and else '0';
+	LD_NPIX <= '1' when SEL_DATA = '1' or CL_XY = '1' else '0';
+	LD_RGB <= '1' when LD_NPIX = '1' else '0';
 	
 	DEC_CNPIX <= '1' when EP = E9 and DONE_COLOUR = '1' else '0';
 	INC_Y <= '1' when EP = E10 and ALL_PIX = '0' else '0';
@@ -131,7 +131,7 @@ architecture arq_lcd_drawing of lcd_drawing is
 	-- REG XCOL: RX
 	RX : process(CLK, RESET_L)
 	begin
-		if RESET_L = '0' then XCOL <= (others => '0'); -- clear registro con seÃ±al reset
+		if RESET_L = '0' then XCOL <= (others => '0'); -- clear registro con seÃÂ±al reset
 		elsif CLK'event and CLK='1' then 			   -- flanco de reloj
 			if LD_XY = '1' then XCOL <= "01000110";
 			elsif CL_XY = '1' then XCOL <= (others => '0');
@@ -142,7 +142,7 @@ architecture arq_lcd_drawing of lcd_drawing is
 	-- REG RGB: RC
 	RC : process(CLK, RESET_L)
 	begin
-		if RESET_L = '0' then RGB <= (others => '0'); -- clear registro con seÃ±al reset
+		if RESET_L = '0' then RGB <= (others => '0'); -- clear registro con seÃÂ±al reset
 		elsif CLK'event and CLK='1' then 			   -- flanco de reloj
 			if LD_XY = '1' then RGB <= DRGB;
 			end if;
@@ -163,7 +163,7 @@ architecture arq_lcd_drawing of lcd_drawing is
 	end process CY;
 	YROW <= std_logic_vector(cnt_YROW);
 
-	--Multiplexor para número de píxels
+	--Multiplexor para nÃºmero de pÃ­xels
 	MUX_PIX <= (others => '0') when RESET_L='0' else
 			"10010110000000000" when SEL_DATA='0' else
 			"00000000000000010";
@@ -173,14 +173,14 @@ architecture arq_lcd_drawing of lcd_drawing is
 	-- REG NUM_PIX: RNPIX
 	RNPIX : process(CLK, RESET_L)
 	begin
-		if RESET_L = '0' then NUM_PIX <= (others => '0'); -- clear registro con seÃ±al reset
+		if RESET_L = '0' then NUM_PIX <= (others => '0'); -- clear registro con seÃÂ±al reset
 		elsif CLK'event and CLK='1' then 			   -- flanco de reloj
 			if LD_NPIX = '1' then NUM_PIX <= MUX_PIX;
 			end if;
 		end if;
 	end process RNPIX;
 
-	--contador píxeles restantes: CNPIX 
+	--contador pÃ­xeles restantes: CNPIX 
 	CNPIX : process(CLK, RESET_L)
 	begin
 		if RESET_L = '0' then u_QPIX <= (others =>'0');
