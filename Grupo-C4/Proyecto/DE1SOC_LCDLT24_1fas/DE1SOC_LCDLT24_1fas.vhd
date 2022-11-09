c7c8ea00cfa7bf15a499a1c3e50fb87ca5a7c92f
Amanda Sin <81495532+Andyuks@users.noreply.github.com>
2022-11-09 17:18:19 +0100

    quartus update

--- Grupo-C4/Proyecto/DE1SOC_LCDLT24_1fas/DE1SOC_LCDLT24_1fas.vhd
+++ Grupo-C4/Proyecto/DE1SOC_LCDLT24_1fas/DE1SOC_LCDLT24_1fas.vhd
@@ -1,3 +1,7 @@
+-------------------------
+-- fichero top: fase 1 --
+-------------------------
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -19,8 +23,7 @@ entity DE1SOC_LCDLT24_1fas is
 	--	SW 			: in	std_logic_vector(9 downto 0);
 
 		-- LEDR ----------------
-		--LEDR 		: out	std_logic_vector(8 downto 0);
-		LEDR 		: out	std_logic_vector(9 downto 9);
+		LEDR 		: out	std_logic_vector(9 downto 0);
 
 		-- LT24_LCD ----------------
 		LT24_LCD_ON     : out std_logic;
@@ -101,67 +104,81 @@ architecture str of DE1SOC_LCDLT24_1fas is
 		);
 	end component;
 	  
-		signal clk,reset,reset_l :  std_logic;
+		signal clk,reset,reset_l 		:  std_logic;
 
-		signal 	LT24_Init_Done: std_logic;
+		-- setup
+		signal 	LT24_Init_Done			: 	std_logic;
 		signal  	LT24_CS_N_Int        :  std_logic;
 		signal  	LT24_RS_Int          :  std_logic;
 		signal  	LT24_WR_N_Int        :  std_logic;
 		signal  	LT24_RD_N_Int        :  std_logic;
 		signal  	LT24_D_Int           :  std_logic_vector(15 downto 0);
 	  
-
-		signal	OP_SETCURSOR :  std_logic;
-		signal	OP_DRAWCOLOUR :  std_logic;
-		signal	DONE_CURSOR :  std_logic;
-		signal	DONE_COLOUR :  std_logic;
-		signal	XCOL :  std_logic_vector(7 downto 0);
-		signal	YROW :  std_logic_vector(8 downto 0);
-		signal	RGB :  std_logic_vector(15 downto 0);
-		signal	NUM_PIX : unsigned(16 downto 0);
-		signal 	LCD_CS_N :  std_logic;
-		signal	LCD_RS :  std_logic;
-		signal	LCD_WR_N :  std_logic;
-		signal	LCD_DATA: std_logic_vector(15 downto 0);
-		signal	LCD_Init_Done :  std_logic;
 	  
-		signal	COLOUR_CODE :  std_logic_vector(2 downto 0);
-		signal 	DEL_SCREEN : std_logic;
-		signal 	DRAW_FIG : std_logic;
+		-- ctrl
+		signal	OP_SETCURSOR 			:  std_logic;
+		signal	OP_DRAWCOLOUR 			:  std_logic;
+		signal	DONE_CURSOR 			:  std_logic;
+		signal	DONE_COLOUR 			:  std_logic;
+		signal	XCOL 						:  std_logic_vector(7 downto 0);
+		signal	YROW 						:  std_logic_vector(8 downto 0);
+		signal	RGB 						:  std_logic_vector(15 downto 0);
+		signal	NUM_PIX 					: 	unsigned(16 downto 0);
+		signal 	LCD_CS_N 				:  std_logic;
+		signal	LCD_RS 					:  std_logic;
+		signal	LCD_WR_N 				:  std_logic;
+		signal	LCD_DATA					: 	std_logic_vector(15 downto 0);
+		signal	LCD_Init_Done 			:  std_logic;
+	  
+		-- drawing
+		signal	COLOUR_CODE 			:  std_logic_vector(2 downto 0);
+		signal 	DEL_SCREEN 				: 	std_logic;
+		signal 	DRAW_FIG 				: 	std_logic;
 	  
 	begin 
 		clk <= CLOCK_50;
-		reset_l<=KEY(0);
 		
-		LT24_RD_N_Int<='1';
+		LT24_RD_N_Int<='1'; -- no usaremos la funcionalidad táctil.
 
+		reset_l<=KEY(0);
 		DEL_SCREEN <= not(KEY(1));
 		DRAW_FIG <= not(KEY(2));
+		
 		COLOUR_CODE <= SW(2 downto 0);
-		LEDR(9)  <= LT24_Init_Done; --para comprobar visualmente q funciona
+		
+		LEDR(0)  <= SW(0); --para comprobar visualmente que el switch está activado
+		LEDR(1)  <= SW(1); --para comprobar visualmente que el switch está activado
+		LEDR(2)  <= SW(2); --para comprobar visualmente que el switch está activado
+		LEDR(3)  <= '0'; 
+		LEDR(4)  <= '0'; 
+		LEDR(5)  <= '0'; 
+		LEDR(6)  <= '0'; 
+		LEDR(7)  <= '0';  
+		LEDR(8)  <= '0'; 
+		LEDR(9)  <= LT24_Init_Done; --para comprobar visualmente que funciona
 		
 		-- Osagaien elkarketa        --------------    
 		--    señal de entrada del componente     => señal a la que se va ha enlazar
 		O1_SETUP:LT24Setup 
 		port map(
-				clk          => clk,
+			clk          => clk,
 			reset_l      => reset_l,
 
+			-- entradas
+			LT24_CS_N_Int       => LT24_CS_N_Int,
+			LT24_RS_Int         => LT24_RS_Int,
+			LT24_WR_N_Int       => LT24_WR_N_Int,
+			LT24_RD_N_Int       => LT24_RD_N_Int,
+			LT24_D_Int          => LT24_D_Int,
+			
+			-- salidas
 			LT24_LCD_ON      => LT24_LCD_ON,
 			LT24_RESET_N     => LT24_RESET_N,
 			LT24_CS_N        => LT24_CS_N,
 			LT24_RS          => LT24_RS,
 			LT24_WR_N        => LT24_WR_N,
 			LT24_RD_N        => LT24_RD_N,
-			LT24_D           => LT24_D,
-
-			LT24_CS_N_Int       => LT24_CS_N_Int,
-			LT24_RS_Int         => LT24_RS_Int,
-			LT24_WR_N_Int       => LT24_WR_N_Int,
-			LT24_RD_N_Int       => LT24_RD_N_Int,
-			LT24_D_Int          => LT24_D_Int,
-				
-			  
+			LT24_D           => LT24_D,  
 			LT24_Init_Done      => LT24_Init_Done
 		 );
 		   
@@ -173,12 +190,14 @@ architecture str of DE1SOC_LCDLT24_1fas is
 			CLK =>  clk,
 			RESET_L => reset_l,
 
+			-- entradas
 			DEL_SCREEN => DEL_SCREEN,
 			DRAW_FIG => DRAW_FIG, 
 			DONE_CURSOR => DONE_CURSOR,
 			DONE_COLOUR =>  DONE_COLOUR,
 			COLOUR_CODE => COLOUR_CODE,
 
+			-- salidas
 			OP_SETCURSOR => OP_SETCURSOR,
 			OP_DRAWCOLOUR => OP_DRAWCOLOUR,
 			XCOL => XCOL,
