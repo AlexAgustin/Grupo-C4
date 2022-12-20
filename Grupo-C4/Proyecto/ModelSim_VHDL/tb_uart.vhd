1d7644772fb071c33a24d04eb3750688ecaa23f3
AlexAgustin <aagustin002@ikasle.ehu.eus>
2022-12-20 22:02:43 +0100

    UART works

--- Grupo-C4/Proyecto/ModelSim_VHDL/tb_uart.vhd
+++ Grupo-C4/Proyecto/ModelSim_VHDL/tb_uart.vhd
@@ -14,23 +14,22 @@ architecture arq_tb_uart of tb_uart is
 	component uart
 	port(
 		CLK, RESET_L: in std_logic;
-		Rx: in std_logic;
+		Rx,DONE_ORDER: in std_logic;
 		VEL: in std_logic_vector(1 downto 0);
 		RTS: in std_logic;
-		DATARECV: out std_logic_vector (7 downto 0);
-		CTS,LED: out std_logic
+		CTS,LED,DRAW_FIG,DEL_SCREEN: out std_logic;
+		COLOUR_CODE: out std_logic_vector(2 downto 0)
 	);
 	end component;
 
 
 	signal CLK : std_logic := '0';
 	signal RESET_L : std_logic := '0';
-	signal Rx : std_logic := '1';
+	signal Rx: std_logic := '1';
 	signal VEL: std_logic_vector (1 downto 0);
-	signal RTS,CTS : std_logic := '0';
-	signal DATARECV: std_logic_vector(7 downto 0);
+	signal RTS,CTS,DRAW_FIG,DEL_SCREEN,DONE_ORDER : std_logic := '0';
 	signal LED: std_logic;
-
+	signal COLOUR_CODE: std_logic_vector(2 downto 0);
 
 	begin
 
@@ -40,8 +39,11 @@ architecture arq_tb_uart of tb_uart is
 		Rx =>Rx,
 		VEL =>VEL,
 		RTS =>RTS,
-		DATARECV =>DATARECV,
+		DRAW_FIG =>DRAW_FIG,
+		DEL_SCREEN =>DEL_SCREEN,
+		COLOUR_CODE =>COLOUR_CODE,
 		CTS =>CTS,
+		DONE_ORDER=>DONE_ORDER,
 		LED =>LED
 	);
 		
@@ -70,11 +72,11 @@ architecture arq_tb_uart of tb_uart is
 		wait for 80 ns;
 
 		RTS <= '0';
-		Rx <='1'; --dato 1
+		Rx <='0'; --dato 1
 
 		wait for 140 ns; --procesa dato 1
 
-		Rx <='0'; --dato 2
+		Rx <='1'; --dato 2
 
 		wait for 140 ns; --procesa dato 2
 
@@ -82,15 +84,15 @@ architecture arq_tb_uart of tb_uart is
 
 		wait for 140 ns; --procesa dato 3
 
-		Rx <='1'; --dato 4
+		Rx <='0'; --dato 4
 
 		wait for 140 ns; --procesa dato 4
 
-		Rx <='1'; --dato 5
+		Rx <='0'; --dato 5
 
 		wait for 140 ns; --procesa dato 5
 
-		Rx <='0'; --dato 6
+		Rx <='1'; --dato 6
 
 		wait for 140 ns; --procesa dato 6
 
@@ -98,33 +100,66 @@ architecture arq_tb_uart of tb_uart is
 
 		wait for 140 ns; --procesa dato 7
 
-		Rx <='1'; --dato 8
+		Rx <='0'; --dato 8
 
 		wait for 140 ns; --procesa dato 8
 
 		Rx <='1'; --parity
 
-		wait for 100 ns; --procesa parity
+		wait for 460 ns;
 
-		Rx <= '0'; 
-
-		wait for 100 ns;
-
-		wait for 80 ns;
+		DONE_ORDER<='1';
 	
 	-------------------------------------------------
 		--prueba trama 2
-		--wait for 20 ns;
-		--RTS <= '1';
+		wait for 100 ns;
 		
-		--wait for 20 ns;
-		--Rx <= '0';
+		DONE_ORDER <= '0';
 
-		--wait for 40 ns;
-		--RTS <= '0';
-		--Rx <='1';
+		wait for 60 ns;
 		
-		--wait for 1000 ns;
+		Rx <= '0';
+
+		wait for 80 ns;
+
+		RTS <= '0';
+		Rx <='0'; --dato 1
+
+		wait for 140 ns; --procesa dato 1
+
+		Rx <='1'; --dato 2
+
+		wait for 140 ns; --procesa dato 2
+
+		Rx <='1'; --dato 3
+
+		wait for 140 ns; --procesa dato 3
+
+		Rx <='0'; --dato 4
+
+		wait for 140 ns; --procesa dato 4
+
+		Rx <='0'; --dato 5
+
+		wait for 140 ns; --procesa dato 5
+
+		Rx <='1'; --dato 6
+
+		wait for 140 ns; --procesa dato 6
+
+		Rx <='1'; --dato 7
+
+		wait for 140 ns; --procesa dato 7
+
+		Rx <='0'; --dato 8
+
+		wait for 140 ns; --procesa dato 8
+
+		Rx <='1'; --parity
+
+		wait for 540 ns;
+	
+		DONE_ORDER<='1';
 	
 	-------------------------------------------------
 
