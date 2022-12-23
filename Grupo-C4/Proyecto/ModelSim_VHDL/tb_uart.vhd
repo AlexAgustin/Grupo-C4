00493cbff5bd8f223d5c43bf6e6d2b1cd4ca3f30
AlexAgustin <aagustin002@ikasle.ehu.eus>
2022-12-23 12:21:30 +0100

    final sesion de hoy

--- Grupo-C4/Proyecto/ModelSim_VHDL/tb_uart.vhd
+++ Grupo-C4/Proyecto/ModelSim_VHDL/tb_uart.vhd
@@ -16,9 +16,9 @@ architecture arq_tb_uart of tb_uart is
 		CLK, RESET_L: in std_logic;
 		Rx: in std_logic;
 		VEL: in std_logic_vector(1 downto 0);
-		DONE_ORDER: in std_logic;--RTS,
-		LED,DRAW_FIG,DEL_SCREEN, DIAG, VERT: out std_logic;--CTS,
-		COLOUR_CODE: out std_logic_vector(2 downto 0)
+		DONE_OP: in std_logic;
+		DAT: out std_logic_vector(7 downto 0);
+		LED,NEWOP: out std_logic
 	);
 	end component;
 
@@ -26,10 +26,11 @@ architecture arq_tb_uart of tb_uart is
 	signal CLK : std_logic := '0';
 	signal RESET_L : std_logic := '0';
 	signal Rx: std_logic := '1';
-	signal VEL: std_logic_vector (1 downto 0);
-	signal DRAW_FIG,DEL_SCREEN,DONE_ORDER , DIAG, VERT: std_logic := '0';--RTS,CTS,
+	signal VEL: std_logic_vector (1 downto 0):="00";
 	signal LED: std_logic;
-	signal COLOUR_CODE: std_logic_vector(2 downto 0);
+	signal DONE_OP: std_logic:='0';
+	signal NEWOP: std_logic;
+	signal DAT: std_logic_vector(7 downto 0);
 
 	begin
 
@@ -38,15 +39,10 @@ architecture arq_tb_uart of tb_uart is
 		RESET_L =>RESET_L,
 		Rx =>Rx,
 		VEL =>VEL,
-		--RTS =>RTS,
-		DRAW_FIG =>DRAW_FIG,
-		DEL_SCREEN =>DEL_SCREEN,
-		DIAG => DIAG,
-		VERT=> VERT,
-		COLOUR_CODE =>COLOUR_CODE,
-		--CTS =>CTS,
-		DONE_ORDER=>DONE_ORDER,
-		LED =>LED
+		LED =>LED,
+		DONE_OP=>DONE_OP,
+		NEWOP=>NEWOP,
+		DAT=>DAT
 	);
 		
 
@@ -68,317 +64,111 @@ architecture arq_tb_uart of tb_uart is
 		
 		Rx <= '0';
 
-		wait for 180 ns;
+		wait for 120 ns;
 
 		Rx <='1'; --dato 1
 
-		wait for 140 ns; --procesa dato 1
+		wait for 120 ns; --procesa dato 1
 
 		Rx <='0'; --dato 2
 
-		wait for 140 ns; --procesa dato 2
+		wait for 120 ns; --procesa dato 2
 
 		Rx <='0'; --dato 3
 
-		wait for 140 ns; --procesa dato 3
+		wait for 120 ns; --procesa dato 3
 
 		Rx <='0'; --dato 4
 
-		wait for 140 ns; --procesa dato 4
+		wait for 120 ns; --procesa dato 4
 
 		Rx <='1'; --dato 5
 
-		wait for 140 ns; --procesa dato 5
+		wait for 120 ns; --procesa dato 5
 
 		Rx <='1'; --dato 6
 
-		wait for 140 ns; --procesa dato 6
+		wait for 120 ns; --procesa dato 6
 
 		Rx <='0'; --dato 7
 
-		wait for 140 ns; --procesa dato 7
+		wait for 120 ns; --procesa dato 7
 
 		Rx <='0'; --dato 8
 
-		wait for 140 ns; --procesa dato 8
+		wait for 120 ns; --procesa dato 8
 
 		Rx <='1'; --stop
 
-		wait for 140 ns; --procesa stop
+		wait for 120 ns; --procesa stop
 
 		Rx <='1'; --stop2
 
-		wait for 460 ns;
+		wait for 200 ns;
 
-		DONE_ORDER<='1';
+		DONE_OP<='1';
+
+		wait for 20 ns;
+
+		DONE_OP<= '0';
 	
 	-------------------------------------------------
 		--prueba trama 2 : draw fig (0110110)
-		wait for 100 ns;
-		
-		DONE_ORDER <= '0';
 
 		wait for 60 ns;
 		
 		Rx <= '0';
 
-		wait for 180 ns;
+		wait for 120 ns;
 
 		Rx <='0'; --dato 1
 
-		wait for 140 ns; --procesa dato 1
+		wait for 120 ns; --procesa dato 1
 
 		Rx <='1'; --dato 2
 
-		wait for 140 ns; --procesa dato 2
+		wait for 120 ns; --procesa dato 2
 
 		Rx <='1'; --dato 3
 
-		wait for 140 ns; --procesa dato 3
+		wait for 120 ns; --procesa dato 3
 
 		Rx <='0'; --dato 4
 
-		wait for 140 ns; --procesa dato 4
+		wait for 120 ns; --procesa dato 4
 
 		Rx <='0'; --dato 5
 
-		wait for 140 ns; --procesa dato 5
+		wait for 120 ns; --procesa dato 5
 
 		Rx <='1'; --dato 6
 
-		wait for 140 ns; --procesa dato 6
+		wait for 120 ns; --procesa dato 6
 
 		Rx <='1'; --dato 7
 
-		wait for 140 ns; --procesa dato 7
+		wait for 120 ns; --procesa dato 7
 
 		Rx <='0'; --dato 8
 
-		wait for 140 ns; --procesa dato 8
+		wait for 120 ns; --procesa dato 8
 
-		Rx <='1'; --stop
+		Rx <='0'; --stop
 
-		wait for 140 ns; --procesa stop
+		wait for 120 ns; --procesa stop
 
 		Rx <='1'; --stop2
 
-		wait for 540 ns;
-	
-		DONE_ORDER<='1';
-	
-	-------------------------------------------------
-		--prueba trama 3 : del screen
 		wait for 100 ns;
+
+		DONE_OP<='1';
+
+		wait for 20 ns;
 		
-		DONE_ORDER <= '0';
+		DONE_OP<= '0';
 
-		wait for 60 ns;
-		
-		Rx <= '0';
+		wait for 200 ns;
 
-		wait for 180 ns;
-
-		Rx <='0'; --dato 1
-
-		wait for 140 ns; --procesa dato 1
-
-		Rx <='1'; --dato 2
-
-		wait for 140 ns; --procesa dato 2
-
-		Rx <='0'; --dato 3
-
-		wait for 140 ns; --procesa dato 3
-
-		Rx <='0'; --dato 4
-
-		wait for 140 ns; --procesa dato 4
-
-		Rx <='0'; --dato 5
-
-		wait for 140 ns; --procesa dato 5
-
-		Rx <='1'; --dato 6
-
-		wait for 140 ns; --procesa dato 6
-
-		Rx <='1'; --dato 7
-
-		wait for 140 ns; --procesa dato 7
-
-		Rx <='0'; --dato 8
-
-		wait for 140 ns; --procesa dato 8
-
-		Rx <='1'; --stop
-
-		wait for 140 ns; --procesa stop
-
-		Rx <='1'; --stop2
-
-		wait for 540 ns;
-	
-		DONE_ORDER<='1';
-	
-	-------------------------------------------------
-		--prueba trama 4 : vert 
-		wait for 100 ns;
-		
-		DONE_ORDER <= '0';
-
-		wait for 60 ns;
-		
-		Rx <= '0';
-
-		wait for 180 ns;
-
-		Rx <='0'; --dato 1
-
-		wait for 140 ns; --procesa dato 1
-
-		Rx <='1'; --dato 2
-
-		wait for 140 ns; --procesa dato 2
-
-		Rx <='1'; --dato 3
-
-		wait for 140 ns; --procesa dato 3
-
-		Rx <='0'; --dato 4
-
-		wait for 140 ns; --procesa dato 4
-
-		Rx <='1'; --dato 5
-
-		wait for 140 ns; --procesa dato 5
-
-		Rx <='1'; --dato 6
-
-		wait for 140 ns; --procesa dato 6
-
-		Rx <='1'; --dato 7
-
-		wait for 140 ns; --procesa dato 7
-
-		Rx <='0'; --dato 8
-
-		wait for 140 ns; --procesa dato 8
-
-		Rx <='1'; --stop
-
-		wait for 140 ns; --procesa stop
-
-		Rx <='1'; --stop2
-
-		wait for 540 ns;
-	
-		DONE_ORDER<='1';	
-	
-	-------------------------------------------------
-		--prueba trama 5 : diag
-		wait for 100 ns;
-		
-		DONE_ORDER <= '0';
-
-		wait for 60 ns;
-		
-		Rx <= '0';
-
-		wait for 180 ns;
-
-		Rx <='0'; --dato 1
-
-		wait for 140 ns; --procesa dato 1
-
-		Rx <='0'; --dato 2
-
-		wait for 140 ns; --procesa dato 2
-
-		Rx <='1'; --dato 3
-
-		wait for 140 ns; --procesa dato 3
-
-		Rx <='0'; --dato 4
-
-		wait for 140 ns; --procesa dato 4
-
-		Rx <='0'; --dato 5
-
-		wait for 140 ns; --procesa dato 5
-
-		Rx <='1'; --dato 6
-
-		wait for 140 ns; --procesa dato 6
-
-		Rx <='1'; --dato 7
-
-		wait for 140 ns; --procesa dato 7
-
-		Rx <='0'; --dato 8
-
-		wait for 140 ns; --procesa dato 8
-
-		Rx <='1'; --stop
-
-		wait for 140 ns; --procesa stop
-
-		Rx <='1'; --stop2
-
-		wait for 540 ns;
-	
-		DONE_ORDER<='1';
-
--------------------------------------------------
-		--prueba trama 6 : error - activacion led!
-		wait for 100 ns;
-		
-		DONE_ORDER <= '0';
-
-		wait for 60 ns;
-		
-		Rx <= '0';
-
-		wait for 180 ns;
-
-		Rx <='0'; --dato 1
-
-		wait for 140 ns; --procesa dato 1
-
-		Rx <='0'; --dato 2
-
-		wait for 140 ns; --procesa dato 2
-
-		Rx <='1'; --dato 3
-
-		wait for 140 ns; --procesa dato 3
-
-		Rx <='0'; --dato 4
-
-		wait for 140 ns; --procesa dato 4
-
-		Rx <='0'; --dato 5
-
-		wait for 140 ns; --procesa dato 5
-
-		Rx <='1'; --dato 6
-
-		wait for 140 ns; --procesa dato 6
-
-		Rx <='1'; --dato 7
-
-		wait for 140 ns; --procesa dato 7
-
-		Rx <='0'; --dato 8
-
-		wait for 140 ns; --procesa dato 8
-
-		Rx <='1'; --stop
-
-		wait for 140 ns; --procesa stop
-
-		Rx <='0'; --stop2
-
-		wait for 540 ns;
 	end process;
 end arq_tb_uart;
 
